82b4bcb27c28fad0fece1698fabb1112e7b68520
unknown <mabg0630@gmail.com>
2025-05-22 22:17:11 -0500

    cors

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://fitness-dash.celenius.store", "http://localhost:5173","https://frontfitnessdashboard.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
